397f6a314e71edc044fa52a3d3dc5196a5f5ae00
Hoyt Summers Pittman <secondsun@gmail.com>
2022-07-05 19:32:45 -0400

    Project opener

--- app/src/main/java/net/saga/a16_bitstudio/data/ContextProjectRepository.kt
+++ app/src/main/java/net/saga/a16_bitstudio/data/ContextProjectRepository.kt
@@ -1,5 +1,6 @@
 package net.saga.a16_bitstudio.data
 
+import android.content.ContentResolver
 import android.content.Context
 import android.net.Uri
 import android.util.Log
@@ -8,9 +9,11 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.File
+import java.io.FileDescriptor
 
 class ContextProjectRepository(
     val context: Context,
+    private val contentResolver :ContentResolver = context.contentResolver,
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : ProjectRepository {
     override fun getDefaultDirectory(): Uri {
@@ -18,12 +21,13 @@ class ContextProjectRepository(
         return Uri.fromFile(context.getExternalFilesDir(null)?:context.filesDir)
     }
 
-    override suspend fun getFilesInDirectory(uri: Uri): List<File> =
+    override suspend fun getFilesInDirectory(uri: Uri): List<FileDescriptor> =
         withContext(defaultDispatcher) {
             Log.i("ContextProjectRepository", "getFilesInDirectory ${uri}")
+            contentResolver.fi
             val file = uri.toFile()
             if (!file.isDirectory) {
-                emptyList<File>()
+                emptyList<FileDescriptor>()
             } else {
                 Log.i("ContextProjectRepository", "${file.listFiles()?.toList()}")
                 file.listFiles()?.toList() ?: emptyList()
--- app/src/main/java/net/saga/a16_bitstudio/ui/App.kt
+++ app/src/main/java/net/saga/a16_bitstudio/ui/App.kt
@@ -1,33 +1,57 @@
 package net.saga.a16_bitstudio.ui
 
-import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
+import net.saga.a16_bitstudio.MainActivity
 import net.saga.a16_bitstudio.data.ContextProjectRepository
 import net.saga.a16_bitstudio.ui.picker.FileViewerVM
 import net.saga.a16_bitstudio.ui.theme.SixteenbitStudioTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun App(context : Context) {
-    var pickerVM = viewModel(modelClass = FileViewerVM::class.java,
-                             factory = FileViewerVM.provideFactory(ContextProjectRepository(context.applicationContext))
-                            )
+fun App(activity: MainActivity, lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current) {
+
+    val contentResolver = activity.contentResolver
+
+    val takeFlags: Int = Intent.FLAG_GRANT_READ_URI_PERMISSION or
+            Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+
+    var pickerVM = viewModel(
+        modelClass = FileViewerVM::class.java,
+        factory = FileViewerVM.provideFactory(ContextProjectRepository(activity.applicationContext))
+    )
 
     pickerVM.refresh(pickerVM.directoryUri.value.buildUpon().appendPath("/Game").build())
 
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.OpenDocumentTree()) {
+        if (it != null) {
+            contentResolver.takePersistableUriPermission(it, takeFlags)
+            pickerVM.refresh(it)
+        }
+    }
+
     SixteenbitStudioTheme {
         // A surface container using the 'background' color from the theme
-        Scaffold(topBar = {StudioAppBar()}) {
-            Column(modifier = Modifier
-                .padding(it)
-                .fillMaxSize()) {
+        Scaffold(topBar = { StudioAppBar(startOpenIntent = { launcher.launch(pickerVM.directoryUri.value) }) }) {
+            Column(
+                modifier = Modifier
+                    .padding(it)
+                    .fillMaxSize()
+            ) {
                 FileViewer(pickerVM)
             }
         }
